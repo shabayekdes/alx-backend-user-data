3e20b0044c15613e372c3b33d419a83312cfb1c2
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-05-26 00:55:35 +0300

    Add login route

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -59,7 +59,8 @@ def before_request() -> None:
         excluded = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded):
             if auth.authorization_header(
